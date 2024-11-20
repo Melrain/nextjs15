1a9a2a84cdb5aabd509b2dbe98cebb24b04a3d31
Melrain <yezyd92@icloud.com>
2024-11-21 01:42:29 +0800

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return <div className="flex flex-row  justify-end text-red-500 ">page</div>;
 };
 
 export default page;
